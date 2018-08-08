4c0bc0c24803fb3a90cf70efe48cfa997620dc49
davidseo <jseo49ut@gmail.com>
2018-08-08 15:48:30 -0400

    defining the read_instruction, turn, and move methods for the Rover class

--- marsRover.rb
+++ marsRover.rb
@@ -11,6 +11,64 @@ class Rover
 
   end
 
+  def read_instruction(instruction)
+
+    instruction.each_char do |char|
+      if char == "L" || "R"
+        self.turn(char)
+      elsif char == "M"
+        self.move
+      end
+    end
+
+  end
+
+  def turn(spin_direction)
+
+    if spin_direction == "L"
+
+      if @direction == "N"
+        @direction = "W"
+      elsif @direction == "S"
+        @direction = "E"
+      elsif @direction == "W"
+        @direction = "S"
+      elsif @direction == "E"
+        @direction = "N"
+      end
+
+    elsif spin_direction == "R"
+
+      if @direction == "N"
+        @direction = "E"
+      elsif @direction == "S"
+        @direction = "W"
+      elsif @direction == "W"
+        @direction = "N"
+      elsif @direction == "E"
+        @direction = "S"
+      end
+    end
+
+  end
+
+  def move
+
+    if @direction == "N"
+      @y += 1
+
+    elsif @direction == "S"
+      @y -= 1
+
+    elsif @direction == "W"
+      @x -= 1
+
+    elsif @direction == "E"
+      @x += 1
+    end
+
+  end
+
 end
 
 
